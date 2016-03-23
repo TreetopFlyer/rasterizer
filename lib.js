38d92bd8cf947dfd53595d0d520834dc077e69d7
Seth Trowbridge <seth111@gmail.com>
2016-03-23 14:02:49 -0400

    re/write methods added to Rasterizer

--- lib.js
+++ lib.js
@@ -1,6 +1,22 @@
 var Rasterizer = {};
 Rasterizer.Instances = [];
-Rasterizer.Create = function(inJQCanvas)
+Rasterizer.CreateIn = function(inJQContainer, inWidth, inHeight)
+{
+	var obj = {};
+	
+	obj.Math = {};
+	obj.Math.Min = [0, 0];
+	obj.Math.Max = [1, 1];
+	obj.JQ = {};
+	obj.JQ.Canvas = $("<canvas width=\""+inWidth+"\" height=\""+inHeight+"\"></canvas>");
+	inJQContainer.append(obj.JQ.Canvas);
+    
+	obj.Canvas = obj.JQ.Canvas.get(0).getContext("2d");
+	
+	Rasterizer.Instances.push(obj);
+	return obj;
+};
+Rasterizer.CreateFrom = function(inJQCanvas)
 {
 	var obj = {};
 	
@@ -14,6 +30,31 @@ Rasterizer.Create = function(inJQCanvas)
 	
 	Rasterizer.Instances.push(obj);
 	return obj;
+};
+Rasterizer.Bake = function(inRasterizer, inJQImage)
+{
+    inRasterizer.Canvas.drawImage(inJQImage.get(0), 0, 0, inRasterizer.Canvas.canvas.width, inRasterizer.Canvas.canvas.height);
+};
+Rasterizer.ArrayRead = function(inRasterizer)
+{
+    var out = [];
+    
+    Rasterizer.Iterate(inRasterizer, function(inMathX, inMathY, inScreenX, inScreenY)
+    {
+        var data = inRasterizer.Canvas.getImageData(inScreenX, inScreenY, 1, 1).data;
+        out.push([data[0]/255, data[1]/255, data[2]/255]);
+    });
+    
+    return out;
+};
+Rasterizer.ArrayWrite = function(inRasterizer, inArray)
+{
+    var counter = -1;
+    Rasterizer.Iterate(inRasterizer, function(inMathX, inMathY, inScreenX, inScreenY)
+    {
+        counter++;
+        return inArray[counter];
+    });
 };
 Rasterizer.FillStyle = function(inRasterizer, inR, inG, inB)
 {
@@ -34,30 +75,7 @@ Rasterizer.ColorSet = function(inRasterizer, inR, inG, inB, inX, inY)
 };
 Rasterizer.Iterate = function(inRasterizer, inFunction)
 {
-	var width = inRasterizer.Canvas.canvas.width;
-	var height = inRasterizer.Canvas.canvas.height
-	var xPos, yPos;
-	var xPerc, yPerc;
-	var xMath, yMath;
-	var output;
-	
-	for(xPos = 0; xPos<width; xPos++)
-	{
-		xPerc = xPos/width;
-		xMath = inRasterizer.Math.Min[0] + xPerc*(inRasterizer.Math.Max[0] - inRasterizer.Math.Min[0]);
-		
-		for(yPos=0; yPos<height; yPos++)
-		{
-			yPerc = yPos/height;
-			yMath = inRasterizer.Math.Min[1] + yPerc*(inRasterizer.Math.Max[1] - inRasterizer.Math.Min[1]);
-			
-			output = inFunction(xMath, yMath);
-			if(output)
-			{
-				Rasterizer.ColorSet(inRasterizer, output[0], output[1], output[2], xPos, yPos)
-			}
-		}
-	}
+    Rasterizer.IterateRow(inRasterizer, 0, inRasterizer.Canvas.canvas.height, inFunction);
 };
 Rasterizer.IterateRow = function(inRasterizer, inStart, inStop, inFunction)
 {
@@ -67,8 +85,6 @@ Rasterizer.IterateRow = function(inRasterizer, inStart, inStop, inFunction)
 	var xPerc, yPerc;
 	var xMath, yMath;
 	var output;
-	
-	
 	
 	for(xPos = 0; xPos<width; xPos++)
 	{
@@ -80,11 +96,11 @@ Rasterizer.IterateRow = function(inRasterizer, inStart, inStop, inFunction)
 			yPerc = yPos/height;
 			yMath = inRasterizer.Math.Min[1] + yPerc*(inRasterizer.Math.Max[1] - inRasterizer.Math.Min[1]);
 			
-			output = inFunction(xMath, yMath);
+			output = inFunction(xMath, yMath, xPos, yPos);
 			if(output)
 			{
 				Rasterizer.ColorSet(inRasterizer, output[0], output[1], output[2], xPos, yPos)
 			}
 		}
 	}
-};  
+};
